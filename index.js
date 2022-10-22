1b840c77802378504cb32cbb5d78ca93b507eba9
Olayiwola Olaoluwa <61384690+Muneerah1612@users.noreply.github.com>
2022-10-22 07:18:17 +0000

    Defined function

---
+++ index.js
@@ -0,0 +1,12 @@
+function play_game() {
+  console.log('welcome')
+  let username=prompt('Hello,please enter your username:')
+  console.log("yay")
+  
+  
+}
+
+// function get_random_number(){
+  
+// }
+play_game()
